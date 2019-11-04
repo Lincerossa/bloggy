10b6de35a4b50ffefffee7422f65602e94914a25
marcello luatti <marcello.luatti@e-globe-solutions.com>
2019-11-04 17:48:55 +0100

    [components] added a cool threejs effect.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,14 @@
-import React from "react"
+import React, { useEffect } from "react"
 import styled from 'styled-components'
 
+import ImageDistorsion from "../../components/src/ImageDistorsion";
 
-export default () => <Page>Hello world!</Page>
+export default () => {
 
+  return (
+    <ImageDistorsion image={'http://www.tplh.net/file/osaka01.jpg'} />
+  )
+}
 
 
 const Page = styled.div`
